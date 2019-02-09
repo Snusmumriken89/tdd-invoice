a9c284427778a46c37c9cf6fc57a5927167096ec
student4 <student4@pcoip.ki.agh.edu.pl>
2019-02-09 12:16:30 +0100

    commit last

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,9 +10,12 @@ public abstract class Product {
 	private final BigDecimal taxPercent;
 
 	protected Product(String name, BigDecimal price, BigDecimal tax) {
-		if (name == null || name == "" || price == null || price.compareTo(BigDecimal.ZERO) < 0){
-			throw new IllegalArgumentException();
-		}
+		if (name == null || name.isEmpty()){
+			throw new IllegalArgumentException("Name is wrong");
+		} else if (price == null || price.compareTo(BigDecimal.ZERO) < 0) {
+			throw new IllegalArgumentException("Tax is wrong");
+		}		
+		
 			this.name = name;
 			this.price = price;
 			this.taxPercent = tax;
